e4b46cf7c875e237375bf88c8536338578c78059
Besmir Deromemaj <47924653+bdem91@users.noreply.github.com>
2019-05-11 21:57:41 -0400

    Add files via upload

---
+++ DDL_FILE.sql
@@ -0,0 +1,56 @@
+CREATE TABLE departments(
+Dept_No varchar(6),
+Dept_Name varchar(30)
+)
+
+
+
+CREATE TABLE managers(
+Dept_No varchar(6),
+Emp_No int,
+From_Date date,
+To_Date date
+)
+
+
+CREATE TABLE dept_employees(
+Emp_No int,
+Dept_No varchar(6),
+From_Date date,
+To_Date date
+)
+
+CREATE TABLE employees(
+Emp_No int,
+Birth_Date date,
+First_Name varchar(25),
+Last_Name varchar(25),
+Gender varchar(1),
+Hire_Date date
+)
+
+
+CREATE TABLE salaries(
+Emp_No int,
+Salary int,
+From_Date date,
+To_Date date
+)
+
+CREATE TABLE title(
+Emp_No int,
+Title varchar(50),
+From_Date date,
+To_Date date
+)
+
+
+
+
+
+
+
+
+
+
+
